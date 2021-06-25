aba6cb2533f4fbe1e7645037d0dc70290c7d52c9
Asma AYARI <aayari@linagora.com>
2021-06-25 03:48:03 +0100

    update all component in sync module to compile markdown file to html

---
+++ src/linagora.esn.sync/app/components/android/android.controller.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const showdown = require('showdown');
+
+require('../../services/mardown-renderer-service.js');
+
+angular.module('linagora.esn.sync')
+
+  .controller('syncAndroidController', function($scope, $sce, markdownRendererService) {
+    $scope.user = {};
+    const self = this;
+
+    self.$onInit = $onInit;
+    function $onInit() {
+      const converter = new showdown.Converter();
+
+      markdownRendererService.getDefaultLanguage().then(language => {
+        const markdownFile = require(`!!html-loader!../../../i18n/guides/android/${language}.md`);
+
+        $scope.html = converter.makeHtml(markdownFile);
+        $scope.newHTML = $scope.html.replaceAll('user.preferredEmail', 'asma1ayari@gmail.com');
+        $scope.input = $sce.trustAsHtml($scope.newHTML);
+      });
+    }
+  });
+
